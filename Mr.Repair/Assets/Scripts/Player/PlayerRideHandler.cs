9e322d761f97bdaa96509302a886a5cfd7135dc6
e1b24064 <e1b24064@oit.ac.jp>
2025-11-03 00:23:47 +0900

    2025/11/03 0:21 辻 プレイヤーがブロックの動きについていくようにした

--- Mr.Repair/Assets/Scripts/Player/PlayerRideHandler.cs
+++ Mr.Repair/Assets/Scripts/Player/PlayerRideHandler.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerRideHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private Transform currentPlatform;
+    private Vector3 lastPlatformPos;
+
+    private void OnCollisionEnter(Collision collision)
     {
-        
+        if (collision.gameObject.GetComponent<RepeatMoverBlock>() ||
+            collision.gameObject.GetComponent<SlideBlock>())
+        {
+            currentPlatform = collision.transform;
+            lastPlatformPos = currentPlatform.position;
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionStay(Collision collision)
     {
-        
+        if (currentPlatform == null) return;
+
+        // 現在のブロック移動量を求める
+        Vector3 platformDelta = currentPlatform.position - lastPlatformPos;
+
+        // プレイヤーをその分だけ動かす
+        transform.position += platformDelta;
+
+        // 次のフレーム用に記録更新
+        lastPlatformPos = currentPlatform.position;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.transform == currentPlatform)
+        {
+            currentPlatform = null;
+        }
     }
 }
