affe25fc27027246abfaf5f103959197aa23c642
辻響 <0131Hibiki-0131@users.noreply.github.com>
2026-01-16 16:10:25 +0900

    feat:設定画面大雑把に完成

--- Mr.Repair/Assets/Scripts/Settings/SettingsManager.cs
+++ Mr.Repair/Assets/Scripts/Settings/SettingsManager.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems; // ボタン選択に必要
 
 public class SettingsManager : MonoBehaviour
 {
@@ -14,14 +15,20 @@ public class SettingsManager : MonoBehaviour
     public Slider bgmSlider;
     public Slider sfxSlider;
 
+    [Header("First Selected Buttons")]
+    public GameObject graphicsTabButton; // GRAPHICSボタンをアサイン
+    public GameObject audioTabButton;    // AUDIOボタンをアサイン
+    public GameObject backToMainButton;  // BACKボタンをアサイン
+    public GameObject startButton;
+
     void Start()
     {
-        // 初期値をスライダーに反映
+        // 音量初期値の反映
         masterSlider.value = SoundManager.Instance.masterVolume;
         bgmSlider.value = SoundManager.Instance.bgmVolume;
         sfxSlider.value = SoundManager.Instance.environmentVolume;
 
-        // スライダーが動いた時のイベント登録
+        // イベント登録
         masterSlider.onValueChanged.AddListener(SoundManager.Instance.SetMasterVolume);
         bgmSlider.onValueChanged.AddListener(SoundManager.Instance.SetBgmVolume);
         sfxSlider.onValueChanged.AddListener(SoundManager.Instance.SetEnvVolume);
@@ -33,23 +40,49 @@ public class SettingsManager : MonoBehaviour
     {
         mainMenu.SetActive(false);
         settingsPanel.SetActive(true);
+
+        // 中身は空の状態にする
+        audioPanel.SetActive(false);
+        graphicsPanel.SetActive(false);
+
+        // 【重要】GRAPHICSボタンを選択状態にする
+        SetFocus(graphicsTabButton);
     }
 
     public void ShowAudioSettings()
     {
         audioPanel.SetActive(true);
         graphicsPanel.SetActive(false);
+
+        // オーディオ設定に入った瞬間、Masterスライダーを選択状態にする
+        // これにより UISelectionHighlighter が反応して背景パネルが表示されます
+        SetFocus(masterSlider.gameObject);
     }
 
     public void ShowGraphicsSettings()
     {
         audioPanel.SetActive(false);
         graphicsPanel.SetActive(true);
+
+        // グラフィックス設定の中身ができたら、その最初の項目を選択するようにする
     }
 
     public void ShowMainMenu()
     {
         mainMenu.SetActive(true);
         settingsPanel.SetActive(false);
+
+        // メインメニューに戻ったら「SETTINGS」ボタンなどを選択状態にする
+        // SetFocus(settingsButton); 
+        SetFocus(startButton);
+    }
+
+    // 指定したオブジェクトにフォーカスを当てるヘルパー関数
+    private void SetFocus(GameObject target)
+    {
+        if (target != null)
+        {
+            EventSystem.current.SetSelectedGameObject(target);
+        }
     }
 }
